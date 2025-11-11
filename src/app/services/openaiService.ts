d971b09696538d33e7977806f862afeb26556d9c
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2025-11-11 13:22:00 +0000

    New OpenAI function for classifying fact vs opinion in an argument

--- src/app/services/openaiService.ts
+++ src/app/services/openaiService.ts
@@ -8,7 +8,7 @@ export async function getAIAnalysisForArgument(argumentText: string) {
     const response = await openai.responses.create({
         prompt: {
             "id": "pmpt_68f0c016b1448190a6d11717cdd84e7c0142635054d01cbe",
-            "version": "4"
+            "version": "5"
         },
         input: [
             { 
@@ -16,9 +16,6 @@ export async function getAIAnalysisForArgument(argumentText: string) {
                 content: argumentText 
             }
         ],
-        reasoning: {
-            "summary": "auto"
-        },
         store: true,
         include: []
     });
@@ -34,4 +31,32 @@ export async function getAIAnalysisForArgument(argumentText: string) {
         isOpinion: answer.isOpinion,
         justification: answer.justification
     };
+}
+
+export async function extractFactualInformationFromComment(argumentText: string) {
+    const response = await openai.responses.create({
+        prompt: {
+            "id": "pmpt_691321c60ba88194bf7aa1ce08718dfe0e634e3c9603cfdc",
+            "version": "2"
+        },
+        input: [
+            { 
+                role: "user", 
+                content: argumentText 
+            }
+        ],
+        store: true,
+        include: []
+    });
+
+    const functionCallItem = response.output.find(item => item.type == "function_call");
+    if (!functionCallItem) {
+        throw new Error('Failed to extract factual information from comment');
+    }
+    const answer = JSON.parse(functionCallItem.arguments);
+
+    return {
+        factualPart: answer.factualPart,
+        justification: answer.justification
+    }
 }
